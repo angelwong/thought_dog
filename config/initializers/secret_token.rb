c5ed8bf8e2ab6f301e3e19577ed38760b7ebadf6
Nathan Hayes-Roth <nathan.hayesroth@gmail.com>
2013-03-31 14:31:56 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThoughtDog::Application.config.secret_token = '147c0a63bdd41abdc2eb3f3d27454a55adc78d434b11c242ebf95fe2f79dcfc615f1240d595dd56391bfb621de80cd49464a0d2997fa74633a38a7e298ae5340'
